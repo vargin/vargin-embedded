b15e8782ee3eb32ae2ca247db8def316426f3979
Aleh Zasypkin <azasypkin@mozilla.com>
2016-02-18 16:25:34 +0100

    Initial revision of "piano" experiment.

---
+++ Tivia-C-TM4C123GH6PM/experiments/piano/include/pll.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include "cortexm4.h"
+
+/**
+ * Structure type to access the System Timer (SysTick).
+ */
+typedef struct
+{
+  volatile uint32_t RCC;
+  volatile uint32_t RCC2;
+  volatile uint32_t RIS;
+} PLLType;
+
+extern void
+PLLInitialize(uint8_t divisor);
