3c12ad51bab3b76454190a30d4ea6949cb80afbe
Aleh Zasypkin <azasypkin@mozilla.com>
2016-04-13 22:28:26 +0100

    [Mikatron] Distinguish 8 commands, 4 with short press + 4 with long press on a single ATTiny85 pin.

--- arduino/mikatron/src/main.cpp
+++ arduino/mikatron/src/main.cpp
@@ -7,6 +7,9 @@
 volatile uint8_t analogLow = 0;
 volatile uint8_t analogHigh = 0;
 volatile uint8_t analogResultChanged = 0;
+volatile uint8_t pinChanged = 0;
+volatile uint16_t previousAction = 0;
+volatile uint32_t actionTime = 0;
 
 void
 startConversion() {
@@ -18,6 +21,11 @@ ISR(ADC_vect) {
   analogHigh = ADCH;
   analogResultChanged = 1;
 }
+
+/*ISR(PCINT0_vect) {
+  pinChanged = 1;
+}*/
+
 
 void
 setHigh(uint8_t pin) {
@@ -38,6 +46,7 @@ initADC() {
   ADMUX |= 1 << MUX1;
 
   // Choose free running mode.
+  //ADCSRB |= (1 << ADTS2) | (1 << ADTS1);
   ADCSRB &= ~((1 << ADTS0) | (1 << ADTS1) | (1 << ADTS2));
 
   ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
@@ -49,11 +58,24 @@ initADC() {
   ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADATE);
 }
 
+void
+printAction(uint16_t action) {
+  softuart_putchar('-');
+  char str[2];
+  itoa(action, str, 10);
+  softuart_puts(str);
+  softuart_putchar('-');
+  startConversion();
+}
+
 int main(void) {
   // Set PB2 to be output.
   DDRB |= 1 << DDB2;
   // Set PB4 as the input.
   DDRB &= ~(1 << DDB4);
+
+ /* PCMSK |= 1 << PCINT4;
+  GIMSK |= 1 << PCIE;*/
 
   softuart_init();
   softuart_turn_rx_off();
@@ -65,22 +87,78 @@ int main(void) {
   startConversion();
 
   while (1) {
-    if (analogResultChanged) {
-      analogResultChanged = 0;
-
-      softuart_putchar('-');
-      char str[5];
-      itoa((analogHigh << 8) | analogLow, str, 10);
-      softuart_puts(str);
-      softuart_putchar('-');
+    if (!analogResultChanged) {
+      continue;
     }
 
-    setHigh(PB2);
-    _delay_ms(500);
+    analogResultChanged = 0;
 
-    setLow(PB2);
-    _delay_ms(500);
+    uint16_t analogResult = (analogHigh << 8) | analogLow;
+    uint8_t action = 0;
 
-    softuart_putchar('+');
+    if (analogResult < 150) {
+      action = 1;
+    } else if (analogResult < 350) {
+      action = 2;
+    } else if (analogResult < 500) {
+      action = 3;
+    } else if (analogResult < 1000) {
+      action = 4;
+    } else {
+      action = 0;
+    }
+
+    // Long press actions.
+    if (actionTime >= 1500) {
+      switch(action) {
+        case 1:
+          action = 5;
+          break;
+        case 2:
+          action = 6;
+          break;
+        case 3:
+          action = 7;
+          break;
+        case 4:
+          action = 8;
+          break;
+        default:
+          break;
+      }
+
+      if (previousAction != action && action > 0) {
+        setHigh(PB2);
+        _delay_ms(30);
+        setLow(PB2);
+      }
+    }
+
+    if (action != previousAction) {
+      // We display action only once button is unpressed.
+      if (action == 0) {
+        printAction(previousAction);
+        actionTime = 0;
+      }
+
+      previousAction = action;
+    } else if (action > 0 && action < 5) {
+      // Increment only when we are still not sure if it's long press.
+      actionTime += 200;
+      _delay_ms(200);
+    }
+
+   /* if (pinChanged) {
+      pinChanged = 0;
+      softuart_putchar('Z');
+    }*/
+
+    //setHigh(PB2);
+    //_delay_ms(500);
+
+    //setLow(PB2);
+    //_delay_ms(500);
+
+    //softuart_putchar('+');
   }
 }
